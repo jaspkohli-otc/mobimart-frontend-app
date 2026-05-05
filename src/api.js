ba0ab9027ed5a923572b8bcce7a99ff2742816d0
jaspkohli-otc <jaspkohli@gmail.com>
2026-05-05 11:06:50 +0300

    Update API URL to production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
+  baseURL: 'https://mobimart-backend-production.up.railway.app/api'
 })
 
 API.interceptors.request.use((config) => {
